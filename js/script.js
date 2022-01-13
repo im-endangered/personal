a281061f454244d97fc7bd4d07a0247ec860f9d2
im-endangered <pankajbhattarai50@gmail.com>
2022-01-13 14:11:34 +0545

    done

--- js/script.js
+++ js/script.js
@@ -73,3 +73,38 @@ new Typewriter('#typewriter1', {
     loop:true,
     cursor:"|"
   });
+
+//   mathi jane button
+  function scrollUp(){
+      const scrollup=document.getElementById('scroll-up')
+
+    if(this.scrollY>=560) {
+        scrollup.classList.add('show-scroll');
+    
+    }
+    else {
+        scrollup.classList.remove('show-scroll');
+    }   
+  }
+  window.addEventListener('scroll',scrollUp)
+
+  const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+
+
+}
+window.addEventListener('scroll', scrollActive)
